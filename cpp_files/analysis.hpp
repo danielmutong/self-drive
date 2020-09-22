1a114fb28b8d4638dc188cef521d8690505d1b71
danielmutong <48365524+danielmutong@users.noreply.github.com>
2020-09-22 16:04:51 -0700

    Update analysis.hpp

--- cpp_files/analysis.hpp
+++ cpp_files/analysis.hpp
@@ -6,6 +6,8 @@
 class analysis
 {
 public:
+  analysis(){};
+  ~analysis(){};
   int control_ana( std::vector<cv::Vec4i> ,float , float , float , float , Mat , single_line  [] );
   int merge_line(single_line [], int ,float, float , float , float , single_line [], Mat );
   void sort_slopes(single_line [], int );
